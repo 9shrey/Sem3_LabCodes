e18040c058e32f482970e20c2615953bc30f5dde
Rushil Jalal <rushiljalal@gmail.com>
2023-09-04 20:01:15 +0530

    Update 1_TripletsGame.c
    I wrote better code lol.
    BTW follow me on github pls

--- DS Lab/Lab1/1_TripletsGame.c
+++ DS Lab/Lab1/1_TripletsGame.c
@@ -1,31 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-	int a[100],i,j,k,n,sum;
-	printf("state the length of the array: ");
-	scanf("%d",&n);
-	printf("Input elements in array: ");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	for(i=0;i<n-2;i++)
-	{
-		for(j=i+1;j<n-1;j++)
-		{
-			for(k=j+1;k<n;k++)
-			{
-				if(i!=k && j!=k && i!=k)
-				{
-					sum=a[i]+a[j]+a[k];
-					if(sum==0)
-					{
-						printf("%d %d %d",a[i],a[j],a[k]);
-						printf("\n");
-					}
-				}
-			}
-		}
-	}
-	return 0;
+    int a[100], n;
+
+    printf("Enter the length of the array: ");
+    scanf("%d", &n);
+
+    printf("Input elements in the array:\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Triplets with a sum of 0:\n");
+
+    for (int i = 0; i < n - 2; i++)
+    {
+        for (int j = i + 1; j < n - 1; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+            {
+                if (a[i] + a[j] + a[k] == 0)
+                {
+                    printf("%d %d %d\n", a[i], a[j], a[k]);
+                }
+            }
+        }
+    }
+
+    return 0;
 }
