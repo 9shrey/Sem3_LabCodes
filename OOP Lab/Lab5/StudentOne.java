2f0e1604e9fe306478b3c320425b9c62f52fcd58
Shrey Singh <125403535+9shrey@users.noreply.github.com>
2023-09-07 15:35:13 +0530

    Update StudentOne.java

--- OOP Lab/Lab5/StudentOne.java
+++ OOP Lab/Lab5/StudentOne.java
@@ -1,7 +1,9 @@
-class StudentOne {
+import java.util.Scanner;
+
+public class Stud1 {
     private String name;
 
-    public StudentOne(String name) {
+    public Stud1(String name) {
         this.name = name;
     }
 
@@ -22,20 +24,20 @@ class StudentOne {
         return name.replaceAll("\\s", "");
     }
 
-    public static void findNamesWithSubstring(StudentOne[] students, String substring) {
+    public static void findNamesWithSubstring(Stud1[] students, String substring) {
         System.out.println("Names containing '" + substring + "':");
-        for (StudentOne student : students) {
+        for (Stud1 student : students) {
             if (student.name.contains(substring)) {
                 System.out.println(student.name);
             }
         }
     }
 
-    public static void sortStudentsAlphabetically(StudentOne[] students) {
+    public static void sortStudentsAlphabetically(Stud1[] students) {
         for (int i = 0; i < students.length - 1; i++) {
             for (int j = i + 1; j < students.length; j++) {
                 if (students[i].name.compareToIgnoreCase(students[j].name) > 0) {
-                    StudentOne temp = students[i];
+                    Stud1 temp = students[i];
                     students[i] = students[j];
                     students[j] = temp;
                 }
@@ -44,22 +46,40 @@ class StudentOne {
     }
 
     public static void main(String[] args) {
-        StudentOne[] students = new StudentOne[4];
-        students[0] = new StudentOne("John Doe");
-        students[1] = new StudentOne("Jane Smith");
-        students[2] = new StudentOne("Alice Johnson");
-        students[3] = new StudentOne("Robert Brown");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of students: ");
+        int numStudents = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
 
-        System.out.println("Initials: " + students[0].extractInitials());
-        System.out.println("Without Whitespace: " + students[1].removeWhitespace());
+        Stud1[] students = new Stud1[numStudents];
 
-        String substring = "John";
+        for (int i = 0; i < numStudents; i++) {
+            System.out.print("Enter name for student " + (i + 1) + ": ");
+            String name = scanner.nextLine();
+            students[i] = new Stud1(name);
+        }
+
+        System.out.println("Initials and Names without Whitespace:");
+        for (Stud1 student : students) {
+            String initials = student.extractInitials();
+            String nameWithoutWhitespace = student.removeWhitespace();
+
+            System.out.println("Name: " + student.name);
+            System.out.println("Initials: " + initials);
+            System.out.println("Without Whitespace: " + nameWithoutWhitespace);
+            System.out.println();
+        }
+
+        System.out.print("Enter a substring to search for in names: ");
+        String substring = scanner.nextLine();
         findNamesWithSubstring(students, substring);
 
         sortStudentsAlphabetically(students);
         System.out.println("Sorted Student Names:");
-        for (StudentOne student : students) {
+        for (Stud1 student : students) {
             System.out.println(student.name);
         }
+
+        scanner.close();
     }
 }
