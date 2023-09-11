6bd6973075d54eec48b65de35d198620d056ab5f
9shrey <9shrey@gmail.com>
2023-09-12 00:34:33 +0530

    q4dslab5

--- DS Lab/Lab5/q4.c
+++ DS Lab/Lab5/q4.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 
 void deleteKSmallerElements(int arr[], int n, int k) {
-    int stack[n]; // Create a stack to store elements
-    int top = -1; // Initialize the top of the stack
+    int stack[n]; 
+    int top = -1; 
 
-    // Traverse the array
+    
     for (int i = 0; i < n; i++) {
-        // If the stack is not empty and k is greater than 0
         while (top != -1 && k > 0 && stack[top] < arr[i]) {
-            top--; // Pop elements from the stack
-            k--;   // Decrement k
+            top--; 
+            k--;   
         }
-        stack[++top] = arr[i]; // Push the current element onto the stack
+        stack[++top] = arr[i]; 
     }
 
-    // If k is still greater than 0, remove the remaining elements from the stack
+   
     while (k > 0) {
         top--;
         k--;
     }
-
-    // Print the modified array
     for (int i = 0; i <= top; i++) {
         printf("%d ", stack[i]);
     }
