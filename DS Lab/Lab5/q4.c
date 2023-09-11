94907aa988d121355301045c7eaf82793d80c9a6
9shrey <9shrey@gmail.com>
2023-09-12 00:33:37 +0530

    commit lab5ds

--- DS Lab/Lab5/q4.c
+++ DS Lab/Lab5/q4.c
@@ -1,77 +1,46 @@
 #include <stdio.h>
-# define MAX 20
-# define true 1
-# define false 0
-/* Structure definition */
-typedef struct{
-	char item[MAX]; int top;
-}stack;
-#include <stdio.h>
-void push(stack *ps,int x);
-int pop(stack *ps);
-int isempty(stack *ps);
-void display(stack *ps);
 
-void main(){
-	stack stack;
-    int arr[100];
-    printf("Enter the number of elements");
-    int n;
+void deleteKSmallerElements(int arr[], int n, int k) {
+    int stack[n]; // Create a stack to store elements
+    int top = -1; // Initialize the top of the stack
+
+    // Traverse the array
+    for (int i = 0; i < n; i++) {
+        // If the stack is not empty and k is greater than 0
+        while (top != -1 && k > 0 && stack[top] < arr[i]) {
+            top--; // Pop elements from the stack
+            k--;   // Decrement k
+        }
+        stack[++top] = arr[i]; // Push the current element onto the stack
+    }
+
+    // If k is still greater than 0, remove the remaining elements from the stack
+    while (k > 0) {
+        top--;
+        k--;
+    }
+
+    // Print the modified array
+    for (int i = 0; i <= top; i++) {
+        printf("%d ", stack[i]);
+    }
+}
+
+int main() {
+    int n, k;
+    printf("Enter the number of elements: ");
     scanf("%d", &n);
-    for(int i =0;i<n;i++){
+
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    int k;
-    printf("input k");
-    scanf("%d",&k);
-	int i=1;
-	push(&stack,arr[0]);
-    while(i<n){
-		if(arr[i]>((&stack)->item[(&stack)->top])&&k>0){
-			pop(&stack);
-			k--;
-		}else{
-			push(&stack,arr[i]);
-			i++;
-		}
 
-	}
+    printf("Enter the value of k: ");
+    scanf("%d", &k);
 
-	display(&stack);
-}
+    deleteKSmallerElements(arr, n, k);
 
-void push(stack *ps,int x){
-	if (ps->top!=MAX-1){
-		ps->top++;
-		ps->item[ps->top]=x;
-	}
-}
-
-
-int pop(stack *ps){
-	if(!isempty(ps))
-		return(ps->item[ps->top--]);
-}
-
-
-int isempty(stack *ps){
-	if (ps->top==-1)
-		return(true);
-	else
-		return(false);
-}
-void display(stack *ps){
-	printf("The stack is \n");
-	stack *proto = ps;
-	int arr[100];
-	int i=0;
-	while(!isempty(proto)){
-		arr[i] = pop(proto);
-		//printf("%d ",pop(proto));
-		i++;
-	}
-	for(int j=i-2;j>=0;j--){
-		printf("%d \t",arr[j]);
-	}
-
+    return 0;
 }
