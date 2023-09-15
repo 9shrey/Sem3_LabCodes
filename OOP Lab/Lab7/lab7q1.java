3fa8eaff365a04885bea820406e9c83e4837b4c3
Shrey Singh <125403535+9shrey@users.noreply.github.com>
2023-09-15 15:01:43 +0530

    Create lab7q1.java

---
+++ OOP Lab/Lab7/lab7q1.java
@@ -0,0 +1,30 @@
+import com.course.structure.Building;
+import com.course.structure.School;
+import com.course.structure.House;
+
+import java.util.Scanner;
+
+public class lab7q1 {
+    public static void main(String args[]) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("1. Building only, 2. House, 3. School ");
+        int input = sc.nextInt();
+        if (input == 1) {
+            Building b = new Building();
+            b.assign();
+            b.display();
+        }
+        else if (input == 2) {
+            House h =  new House();
+            h.assign();
+            h.display();
+        }
+        else if (input == 3) {
+            School s = new School();
+            s.assign();
+            s.display();
+        }
+        
+    }
+}
