c09eb3ce9b0524ed2ce2c8a9e7154722eeaf831e
Shrey Singh <125403535+9shrey@users.noreply.github.com>
2023-10-09 23:36:41 +0530

    Update q2.c

--- DS Lab/Lab8/q2.c
+++ DS Lab/Lab8/q2.c
@@ -7,61 +7,77 @@ typedef struct {
 } String;
 
 typedef struct {
-    String * q;
-    int front,rear;
+    String* q;
+    int front, rear;
+    int maxSize;
 } Queue;
 
-void enqueue(Queue *q, String s) {
-    if (q->rear == -1) 
+void enqueue(Queue* q, String s) {
+    if (q->rear == q->maxSize - 1) {
+        printf("Queue is full. Cannot enqueue.\n");
+        return;
+    }
+    if (q->rear == -1)
         q->front = 0;
-    strcpy((q->q[++(q->rear)]).str,s.str);
+    strcpy((q->q[++(q->rear)]).str, s.str);
 }
 
-void prioEnqueue(Queue *q, String s) {
-    int i;
-    for(i=q->rear;i>=q->front;i--) 
-        q->q[i+1] = q->q[i];
+void prioEnqueue(Queue* q, String s) {
+    if (q->rear == q->maxSize - 1) {
+        printf("Queue is full. Cannot enqueue.\n");
+        return;
+    }
+    for (int i = q->rear; i >= q->front; i--)
+        q->q[i + 1] = q->q[i];
     q->rear++;
-    strcpy((q->q[q->front]).str,s.str);
+    strcpy((q->q[q->front]).str, s.str);
 }
 
-String dequeue(Queue *q) {
+String dequeue(Queue* q) {
     String s;
-    strcpy(s.str,(q->q[0]).str);
-    int i,ind=0;
-    for(i=1;i<=q->rear;i++) 
-        q->q[ind++] = q->q[i];
+    strcpy(s.str, (q->q[0]).str);
+    int i;
+    for (i = 1; i <= q->rear; i++)
+        q->q[i - 1] = q->q[i];
     q->rear--;
     return s;
 }
 
 int main() {
-    int inp,n;
+    int inp, n;
     String t;
     Queue q;
     q.front = -1;
     q.rear = -1;
-    printf("Enter max size of queue ");
-    scanf("%d",&n);
-    q.q = (String *) malloc(n * sizeof(String));
-    printf("1. Enqueue, 2. Priority Enqueue, 3. Dequeue, -1. Exit");
+    printf("Enter max size of queue: ");
+    scanf("%d", &n);
+    q.maxSize = n;
+    q.q = (String*)malloc(n * sizeof(String));
+    printf("1. Enqueue, 2. Priority Enqueue, 3. Dequeue, 4. Exit\n");
     do {
-        printf("\nEnter choice ");
-        scanf("%d",&inp);
+        printf("Enter choice: ");
+        scanf("%d", &inp);
 
         if (inp == 1) {
-            printf("Enter word to enqueue ");
-            scanf("%s",t.str);
-            enqueue(&q,t);
+            printf("Enter word to enqueue: ");
+            scanf("%s", t.str);
+            enqueue(&q, t);
+        } else if (inp == 2) {
+            printf("Enter word to priority enqueue: ");
+            scanf("%s", t.str);
+            prioEnqueue(&q, t);
+        } else if (inp == 3) {
+            if (q.front == -1) {
+                printf("Queue is empty. Cannot dequeue.\n");
+            } else {
+                printf("Word dequeued is: %s\n", dequeue(&q).str);
+            }
+        } else {
+            inp = 4;
         }
-        else if (inp == 2) {
-            printf("Enter word to enqueue ");
-            scanf("%s",t.str);
-            prioEnqueue(&q,t);
-        }
-        else if (inp == 3) 
-            printf("Word dequeued is : %s",dequeue(&q).str);
-        else
-            inp = -1;
-    } while(inp != -1);
+    } while (inp != 4);
+
+    free(q.q);
+
+    return 0;
 }
