55a4d195ed61718bc5ec5a3cbbf8ed1e91e0aea9
Shrey Singh <125403535+9shrey@users.noreply.github.com>
2023-10-17 09:33:03 +0530

    Create Q4.c

---
+++ DS Lab/Lab9/Q4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LL.h"
+
+void deleteDuplicates(Node *list) {
+    if (list == NULL) {
+        printf("List is empty");
+        return;
+    }
+    Node * iter1 = (Node *) malloc(sizeof(Node));
+    Node * iter2 = (Node *) malloc(sizeof(Node));
+    iter1 = (list)->next;
+    iter2 = (list)->next;
+    while (iter1->next != NULL) {
+        int x = iter1->val;
+        iter2 = iter1->next;
+        while (iter2 != NULL) {
+            if (iter2->val != x) 
+                break;
+            else 
+                iter2 = iter2 -> next;
+        }
+        if (iter2 == NULL) {
+            iter1->next = NULL;
+            break;
+        }
+        else if (iter2 != iter1->next) {
+            iter1->next = iter2;
+        }
+        iter1 = iter1->next;
+    }
+}
+
+int main() {
+    int n,x;
+    Node * list = (Node *) malloc(sizeof(Node));
+    printf("Enter length of list ");
+    scanf("%d",&n);
+    printf("Enter input for list:\n");
+    input(list,n);
+    Node * head = (Node *) malloc(sizeof(Node));
+    head->next = list;
+    printf("After removing duplicates: ");
+    deleteDuplicates(head);
+    display(head->next);
+}
